72011b069620574d9c99cb5e03015c12ca7a2525
chkui <chenkui@chkui.com>
2017-06-22 18:13:38 +0800

    modiefy demo

--- demo/myProServer.js
+++ demo/myProServer.js
@@ -1,6 +1,6 @@
 /**
  * Created by chkui on 2017/6/21.
  */
-const server = require('../src/server'),
+const server = require('../server'),
     config = require('./config');
 server(config);
--- demo/myServer.js
+++ demo/myServer.js
@@ -1,7 +1,7 @@
 /**
  * Created by chkui on 2017/6/21.
  */
-const devServer = require('../src/devServer'),
+const devServer = require('../devServer'),
     reducerDemo = require('./src/reducer'),
     routes = require('./src/routes'),
     App = require('./src/app')
